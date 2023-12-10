b0586120741faaaa0c005d5c4f5c041e1241331f
LET <bjs.6300.137@gmail.com>
2023-12-10 15:57:02 +0900

    Initial Commit

---
+++ githubCommitPra/githubCommitPraApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubCommitPraApp.swift
+//  githubCommitPra
+//
+//  Created by 津本拓也 on 2023/12/10.
+//
+
+import SwiftUI
+
+@main
+struct githubCommitPraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
